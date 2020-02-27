db6c117f76624a5a66255b0b4fc423f89b339ec4
milin bhade <milinbhade1214@gmail.com>
2020-02-27 20:31:37 +0530

    set allowed host

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 DEBUG = os.environ.get('DJANGO_DEBUG','') != 'False'
 
 
-ALLOWED_HOSTS = ['glacial-sands-88692.herokuapp.com']
+ALLOWED_HOSTS = ['glacial-sands-88692.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
